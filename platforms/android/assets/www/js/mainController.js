a4c8cea746111aab31ab29c5a61daa25b9b4482f
Omar Alejandro Chacin Ortega <omar.chacin@sigmagroup.no>
2015-03-21 13:15:20 +0100

    Wrap angular module in Device ready. App is ready to work with HUE

--- platforms/android/assets/www/js/mainController.js
+++ platforms/android/assets/www/js/mainController.js
@@ -7,7 +7,6 @@ angular
 function MainController() {
     /* jshint validthis: true */
     var vm = this;
-
     vm.activate = activate;
     vm.title = 'MainController';
 
@@ -16,6 +15,31 @@ function MainController() {
     ////////////////
 
     function activate() {
+        console.log('Start working with HUE');
+        var hue = jsHue();
+
+        hue.discover(
+            function(bridges) {
+                if(bridges.length === 0) {
+                    console.log('No bridges found. :(');
+                }
+                else {
+                    bridges.forEach(function(b) {
+                        console.log('b is ', b);
+                        console.log('Bridge found at IP address %s.', b.internalipaddress);
+                    });
+                }
+            },
+            function(error) {
+                console.error(error.message);
+            }
+        );
+
+
+        var user = hue.bridge('192.168.1.160').user('nith');
+
+        // create user account (requires link button to be pressed)
+        user.create('nith application');
     }
 
 
