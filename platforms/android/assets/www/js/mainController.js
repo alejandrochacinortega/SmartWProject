e4296c17524ce132e5a4fe7ec72a51506d8244b7
Omar Alejandro Chacin Ortega <omar.chacin@sigmagroup.no>
2015-03-21 14:15:19 +0100

    push to leonid

--- platforms/android/assets/www/js/mainController.js
+++ platforms/android/assets/www/js/mainController.js
@@ -10,67 +10,122 @@ function MainController() {
     vm.activate = activate;
     vm.title = 'MainController';
 
-    vm.setLight = setLight;
-    vm.getLight = getLight;
-
-
-    /////FUNCTIONS
-
-    function setLight() {
-        console.log('Setting light');
-    }
-
-    function getLight() {
-        console.log('Getting light');
-    }
-
-
-
-
-
-    /////FUNCTIONS
-
-
     activate();
 
-    ////////////////
+    console.log('Start working with HUE');
+    var hue = jsHue(),
+        user = null;
 
-    function activate() {
-        console.log('Start working with HUE');
-        var hue = jsHue();
-
-        hue.discover(
-            function(bridges) {
-                if(bridges.length === 0) {
-                    console.log('No bridges found. :(');
-                }
-                else {
-                    bridges.forEach(function(b) {
-                        console.log('b is ', b);
-                        console.log('Bridge found at IP address %s.', b.internalipaddress);
-                    });
-                }
-            },
-            function(error) {
-                console.error(error.message);
+    hue.discover(
+        function(bridges) {
+            if(bridges.length === 0) {
+                console.log('No bridges found. :(');
             }
-        );
+            else {
+                bridges.forEach(function(b) {
+                    console.log('b is ', b);
+                    console.log('Bridge found at IP address %s.', b.internalipaddress);
+                    user = getUser(bridges);
+                    console.log('User is: ', user);
+                    onLightsReady();
+
+                });
+            }
+        },
+        function(error) {
+            console.error(error.message);
+        }
+    );
+
+    function getUser(bridges) {
+        var user = null;
+
+        bridges.forEach(function (bridge) {
+            user = hue.bridge(bridge.internalipaddress).user('newdeveloper');
+            return false;
+        });
+
+        return user;
+    }
+
+    function onLightsReady() {
+        user.setLightState(2, {
+            hue: 56470,
+            sat: 255,
+            bri: 102,
+            on: false
+        });
+        user.setLightState(3, {
+            hue: 56470,
+            sat: 255,
+            bri: 102,
+            on: false
+        });
+        user.setLightState(1, {
+            hue: 56470,
+            sat: 255,
+            bri: 102,
+            on: false
+        });
+
+    }
+
+    /*var user = hue.bridge('192.168.1.160').user('foo');
+    console.log('user ', user);*/
 
 
-        var user = hue.bridge('192.168.1.160').user('nith');
-        console.log('user ', user)
+    /*vm.setLight = user.setLight;*/
 
+
+
+    /*vm.getLights = user.getLights(getLightsSuccess, getLightsFailure);
+
+    function getLightsSuccess(data) {
+        console.log('Getting lights ', data)
+    }
+
+    function getLightsFailure(err) {
+        console.log('ERROR getting lights ', err)
+    }
+
+
+    vm.createUser = createUser;*/
+
+
+    /////FUNCTIONS
+/*
+    function createUser() {
         // create user account (requires link button to be pressed)
-        user.create('nith application', successUser, errorUser);
+        user.create('foo application', successUser, errorUser);
 
         function successUser(data) {
-            console.log('data ', data)
+            console.log('data ', data);
             console.log('user has been created ');
         }
 
         function errorUser(error) {
             console.log('Error creating the user: ', error);
         }
+    }
+
+    function setLight() {
+        console.log('Setting light');
+    }
+
+    function getLights() {
+        console.log('Getting lights');
+    }*/
+
+
+
+
+
+    /////FUNCTIONS
+
+    ////////////////
+
+    function activate() {
+
     }
 
 
--- www/js/mainController.js
+++ www/js/mainController.js
@@ -10,11 +10,103 @@ function MainController() {
     vm.activate = activate;
     vm.title = 'MainController';
 
-    vm.setLight = setLight;
-    vm.getLights = getLights;
+    activate();
+
+    console.log('Start working with HUE');
+    var hue = jsHue(),
+        user = null;
+
+    hue.discover(
+        function(bridges) {
+            if(bridges.length === 0) {
+                console.log('No bridges found. :(');
+            }
+            else {
+                bridges.forEach(function(b) {
+                    console.log('b is ', b);
+                    console.log('Bridge found at IP address %s.', b.internalipaddress);
+                    user = getUser(bridges);
+                    console.log('User is: ', user);
+                    onLightsReady();
+
+                });
+            }
+        },
+        function(error) {
+            console.error(error.message);
+        }
+    );
+
+    function getUser(bridges) {
+        var user = null;
+
+        bridges.forEach(function (bridge) {
+            user = hue.bridge(bridge.internalipaddress).user('newdeveloper');
+            return false;
+        });
+
+        return user;
+    }
+
+    function onLightsReady() {
+        user.setLightState(2, {
+            hue: 56470,
+            sat: 255,
+            bri: 102,
+            on: false
+        });
+        user.setLightState(3, {
+            hue: 56470,
+            sat: 255,
+            bri: 102,
+            on: false
+        });
+        user.setLightState(1, {
+            hue: 56470,
+            sat: 255,
+            bri: 102,
+            on: false
+        });
+
+    }
+
+    /*var user = hue.bridge('192.168.1.160').user('foo');
+    console.log('user ', user);*/
+
+
+    /*vm.setLight = user.setLight;*/
+
+
+
+    /*vm.getLights = user.getLights(getLightsSuccess, getLightsFailure);
+
+    function getLightsSuccess(data) {
+        console.log('Getting lights ', data)
+    }
+
+    function getLightsFailure(err) {
+        console.log('ERROR getting lights ', err)
+    }
+
+
+    vm.createUser = createUser;*/
 
 
     /////FUNCTIONS
+/*
+    function createUser() {
+        // create user account (requires link button to be pressed)
+        user.create('foo application', successUser, errorUser);
+
+        function successUser(data) {
+            console.log('data ', data);
+            console.log('user has been created ');
+        }
+
+        function errorUser(error) {
+            console.log('Error creating the user: ', error);
+        }
+    }
 
     function setLight() {
         console.log('Setting light');
@@ -22,7 +114,7 @@ function MainController() {
 
     function getLights() {
         console.log('Getting lights');
-    }
+    }*/
 
 
 
@@ -30,47 +122,10 @@ function MainController() {
 
     /////FUNCTIONS
 
-
-    activate();
-
     ////////////////
 
     function activate() {
-        console.log('Start working with HUE');
-        var hue = jsHue();
 
-        hue.discover(
-            function(bridges) {
-                if(bridges.length === 0) {
-                    console.log('No bridges found. :(');
-                }
-                else {
-                    bridges.forEach(function(b) {
-                        console.log('b is ', b);
-                        console.log('Bridge found at IP address %s.', b.internalipaddress);
-                    });
-                }
-            },
-            function(error) {
-                console.error(error.message);
-            }
-        );
-
-
-        var user = hue.bridge('192.168.1.160').user('nith');
-        console.log('user ', user)
-
-        // create user account (requires link button to be pressed)
-        user.create('nith application', successUser, errorUser);
-
-        function successUser(data) {
-            console.log('data ', data)
-            console.log('user has been created ');
-        }
-
-        function errorUser(error) {
-            console.log('Error creating the user: ', error);
-        }
     }
 
 
