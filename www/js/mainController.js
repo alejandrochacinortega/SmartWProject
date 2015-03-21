a1f7d8181f9d6cef713a85b39eedd559c82d19d6
Omar Alejandro Chacin Ortega <omar.chacin@sigmagroup.no>
2015-03-21 17:01:46 +0100

    It worksgit add .! Clean code left

--- www/js/mainController.js
+++ www/js/mainController.js
@@ -9,8 +9,106 @@ function MainController() {
     var vm = this;
     vm.activate = activate;
     vm.title = 'MainController';
+    vm.token = null;
+    vm.number_of_current_events = 0;
 
     activate();
+
+    //////////////////////////////////////// GOOGLE ////////////////////////////////////////
+
+    vm.login = login;
+
+    function login() {
+
+        $.oauth2({
+            auth_url: 'https://accounts.google.com/o/oauth2/auth',           // required
+            response_type: 'code',      // required - "code"/"token"
+            token_url: 'https://accounts.google.com/o/oauth2/token',          // required if response_type = 'code'
+            logout_url: 'https://accounts.google.com/logout',         // recommended if available
+            client_id: '435124131375-571j8bianm4e2rlp5ib5qicj3p54l0hm.apps.googleusercontent.com',          // required
+            client_secret: 'ozILwWwXLpmNsgB1yCBiqgPU',      // required if response_type = 'code'
+            redirect_uri: 'http://localhost/oauth2callback',       // required - some dummy url
+            other_params: {
+                'scope': 'https://www.googleapis.com/auth/calendar'
+            }        // optional params object for scope, state, display...
+        }, on_success, function(error, response){
+
+            // do something with error object
+            $("#logs").append("<p class='error'><b>error: </b>"+JSON.stringify(error)+"</p>");
+            $("#logs").append("<p class='error'><b>response: </b>"+JSON.stringify(response)+"</p>");
+        });
+
+    }
+
+    function on_success(token, response) {
+        checkToken(token);
+        vm.token = token;
+    }
+
+
+    function checkToken(token) {
+        console.log('Token on success ', token );
+        $.get('https://www.googleapis.com/calendar/v3/calendars/testfornith@gmail.com/events?' + 'access_token=' + token, ev_handler);
+
+        function ev_handler(data) {
+
+            busyChecker(data);
+        }
+
+        return vm.token = token;
+    }
+
+    function busyChecker(data) {
+        console.log('Data', data);
+        var today = new Date();
+        for (var i = 0; i < data.items.length; i++) {
+            vm.number_of_current_events = 0;
+            var eventStartDate = new Date(data.items[i].start.dateTime);
+            var eventEndDate = new Date(data.items[i].end.dateTime);
+
+
+            if (+today >= +eventStartDate.valueOf() && +today <= +eventEndDate.valueOf()) {
+
+                $("#logs").append("<p class='success'><b>All the events with token: </b>" + JSON.stringify(data.items[i].summary) + "</p>");
+                vm.number_of_current_events += 1;
+                busyUser();
+            }
+        }
+        if (vm.number_of_current_events == 0) {
+            $("#logs").append("<p class='success'><b>All the events with token: </b>" + JSON.stringify('No current events') + "</p>");
+            freeUser();
+
+        }
+
+        return vm.number_of_current_events;
+    }
+
+
+
+
+    vm.test = test;
+
+    function test() {
+        console.log('Global value of token: ', vm.token);
+    }
+
+
+    window.setInterval(function(){
+        /// call your function here
+        console.log('Calling every 5 seg');
+        if (vm.token) {
+            checkToken(vm.token);
+        }
+        else {
+            console.log('Waiting for token');
+        }
+    }, 5000);
+
+
+
+    ///////////////////////////////////////// HUE /////////////////////////////////////////
+
+
 
     console.log('Start working with HUE');
     var hue = jsHue();
@@ -42,7 +140,7 @@ function MainController() {
 
 
     /*VARIABLES*/
-    vm.toggle = false;
+    vm.toggle = true;
     /*Google calendar*/
     vm.busy = false;
 
@@ -114,6 +212,13 @@ function MainController() {
     }
 
 
+    function freeUser() {
+        user.setLightState(3, { on: vm.toggle, xy: [ 0.4084, 0.5168 ] }); /*GREEN LIGHT*/
+    }
+
+    function busyUser() {
+        user.setLightState(3, { on: vm.toggle, xy: [ 0.6736, 0.3221] }); /*RED LIGHT*/
+    }
 
     /////FUNCTIONS
 
